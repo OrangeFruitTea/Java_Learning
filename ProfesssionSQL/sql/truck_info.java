1377848f2cd49fff338a1092ba3fc52a8a7f02df
OrangeFruitTea <118669479+OrangeFruitTea@users.noreply.github.com>
2023-10-13 15:37:05 +0800

    update demo about SQL reading in Java truck_info class initialize

--- ProfesssionSQL/sql/truck_info.java
+++ ProfesssionSQL/sql/truck_info.java
@@ -5,7 +5,6 @@ import com.sun.org.apache.xpath.internal.objects.XString;
 public class truck_info {
     private int _truckId;
     private StorageTypes _storageType;
-
     private char[] _usingRange;
     private double _netWeight;
     private double _sizeLength;
@@ -22,6 +21,7 @@ public class truck_info {
         this._truckId = truckId;
         this._storageType = storageType;
         this._usingRange = usingRange;
+        this._netWeight = netWeight;
         this._sizeLength = sizeLength;
         this._sizeWidth = sizeLength;
         this._sizeHeight = sizeHeight;
@@ -32,5 +32,63 @@ public class truck_info {
     public int get_truckId() {
         return _truckId;
     }
+    public StorageTypes get_storageType() {
+        return _storageType;
+    }
+    public char[] get_usingRange() {
+        return _usingRange;
+    }
+    public double get_netWeight() {
+        return _netWeight;
+    }
+    public double get_sizeLength() {
+        return _sizeLength;
+    }
+    public double get_sizeWidth() {
+        return _sizeWidth;
+    }
+    public double get_sizeHeight() {
+        return _sizeHeight;
+    }
+    public double get_maxLoadVolume() {
+        return _maxLoadVolume;
+    }
+    public double get_maxLoadWeight() {
+        return _maxLoadWeight;
+    }
+    public void set_truckId(int truckId) {
+        this._truckId = truckId;
+    }
+    public void set_storageType(StorageTypes storageType) {
+        this._storageType = storageType;
+    }
+
+    public void set_usingRange(char[] _usingRange) {
+        this._usingRange = _usingRange;
+    }
+    public void set_netWeight(double _netWeight) {
+        this._netWeight = _netWeight;
+    }
+
+    public void set_maxLoadVolume(double _maxLoadVolume) {
+        this._maxLoadVolume = _maxLoadVolume;
+    }
+
+    public void set_maxLoadWeight(double _maxLoadWeight) {
+        this._maxLoadWeight = _maxLoadWeight;
+    }
+
+    public void set_sizeHeight(double _sizeHeight) {
+        this._sizeHeight = _sizeHeight;
+    }
+
+    public void set_sizeLength(double _sizeLength) {
+        this._sizeLength = _sizeLength;
+    }
+
+    public void set_sizeWidth(double _sizeWidth) {
+        this._sizeWidth = _sizeWidth;
+    }
+
 
 }
